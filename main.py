dc8d89a21f3f63cebb2fac50ad1dcd030718da41
Noah <nssimcox@gmail.com>
2023-11-03 19:41:32 -0400

    added basic ai

---
+++ main.py
@@ -0,0 +1,12 @@
+import openai
+
+
+openai.api_key = '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'
+
+Keywords = ["strength", "dexterity", "constitution", "intelligence", "speed", "wisdom", "charisma"]
+
+response = openai.Completion.create(
+    engine="4K context",  # the engine is use
+    prompt="Interpret the following Dungeons and Dragons character description: 'A strong and fast warrior with great knowledge of nature.' Output the words on this list that pertain to the character description.",
+    max_tokens=1000
+)
