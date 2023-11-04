03950a92bf10aa994164ec8011419d75c0fa8f45
Noah <nssimcox@gmail.com>
2023-11-04 00:47:16 -0400

    using chat completions

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import openai
 
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 Keywords = ["strength", "dexterity", "constitution", "intelligence", "speed", "wisdom", "charisma"]
 
-response = openai.Completion.create(
-    engine="4K context",  # the engine in use
-    prompt="Interpret the following Dungeons and Dragons character description: 'A strong and fast warrior with great knowledge of nature.' Output the words on this list that pertain to the character description.",
-    max_tokens=1000
-)
+def generate_Dnd_character(messages):
+    response = openai.Completion.create(
+        model="gpt-3.5-turbo",  # the engine in use
+        messages=messages
+    )
+
+    return response.choices[0].message["content"]
+
+prompt = [{"role": "user", "content": "Interpret the following Dungeons and Dragons character description: 'A strong and fast warrior with great knowledge of nature.' Output the words on this list [" + ', '.join(Keywords) + "] that pertain to the character description."}]
+
+response = generate_Dnd_character(prompt)
+
+print(response)
