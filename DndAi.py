b476702324cf07b1325c5dbac1c3f9baabb8e935
Noah <nssimcox@gmail.com>
2023-11-04 21:24:47 -0400

    big changes

--- DndAi.py
+++ DndAi.py
@@ -3,11 +3,11 @@ import HahaCantGetMySecretKey
 
 openai.api_key = HahaCantGetMySecretKey.openai.api_key
 
-prompt = "This short woman has a long beard with heavy armor. Her convictions come from the gods. She is clumsy, but is very resialnt to damage."
+prompt = "a gnome with bright blue hair and a mischievous twinkle in her eye. She specializes in illusion magic, creating intricate and realistic illusions to confound and deceive her foes. Her spellbook is adorned with colorful drawings and secret notes, reflecting her creative and curious nature as she delves into the world of arcane magic."
 
 keywords = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
 
-gp = 135
+gp = 125
 
 
 dnd_class_prompt = f"Interpret the following level one Dungeons and Dragons character description: '{prompt}' determine a suitable class, from Dnd 5e, for this character and output just that class name. It must only be one word."
@@ -20,13 +20,13 @@ dnd_class = openai.ChatCompletion.create(
 dnd_class_var = dnd_class.choices[0].message["content"]
 print(dnd_class_var)
 
-if dnd_class_var == "Wizard" or "Warlock" or "Rogue" or "Artificer":
+if dnd_class_var in ["Wizard", "Warlock", "Rogue", "Artificer"]:
     gp -= 25
-elif dnd_class_var == "Sorcerer":
+elif dnd_class_var in ["Sorcerer"]:
     gp -= 50
-elif dnd_class_var == "Barbarian" or "Druid":
+elif dnd_class_var in ["Barbarian", "Monk"]:
     gp -= 75
-elif dnd_class_var == "Monk":
+elif dnd_class_var in ["Monk"]:
     gp -= 112
 
 
@@ -40,7 +40,7 @@ raw_stat_rankings = openai.ChatCompletion.create(
 raw_stat_rankings_response = raw_stat_rankings.choices[0].message["content"]
 
 
-refined_stat_rankings_prompt = f"Take this text:{raw_stat_rankings_response} and only output the ability names, in order, on one line."
+refined_stat_rankings_prompt = f"Take this text:{raw_stat_rankings_response} and only output the ability names, in order, on one line, with no numbers."
 
 refined_stat_rankings = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
@@ -62,7 +62,7 @@ dnd_race_var = dnd_race.choices[0].message["content"]
 print(dnd_race_var)
 
 
-dnd_profs_prompt = f"Using this prompt: '{prompt}' determine 6 suitable skill proficiencies, from Dnd 5e, for this character and output just those 6 skill proficeincies on one line. Do not include saving throws, armor, or weapon proficiencies."
+dnd_profs_prompt = f"Using this prompt: '{prompt}' determine 6 suitable skill proficiencies, from Dnd 5e, for this character and output only those 6 skill proficeincies on one line. Do not include saving throws, armor, or weapon proficiencies."
 
 dnd_profs = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
@@ -72,14 +72,54 @@ dnd_profs = openai.ChatCompletion.create(
 dnd_profs_list = dnd_profs.choices[0].message["content"].split()
 print(dnd_profs_list)
 
+if dnd_class_var not in ["Wizard", "Warlock", "Rogue", "Artificer", "Monk"]:
+    dnd_armor_prompt = f"Using this prompt: '{prompt}' determine an armor, from Dnd 5e, for this character. The armor must be less then {gp} gold coins to be a valid output and the output must only be the armor name."
 
-dnd_armor_prompt = f"Using this prompt: '{prompt}' determine an armor, from Dnd 5e, for this character. The armor must be less then {gp} gold coins to buy. Output just the armor name and nothing else."
+    dnd_armor = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": dnd_armor_prompt}]
+    )
 
-dnd_armor = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": dnd_armor_prompt}]
-)
+    if len(dnd_armor.choices[0].message["content"].split()) > 3:
+        count = 5
+        dnd_armor_var = dnd_armor.choices[0].message["content"]
+        while len(dnd_armor_var.split()) > 3 and count > 0:
+            dnd_armor_var = dnd_armor.choices[0].message["content"]
+            count -= 1
+            print(count)
+    else:
+        dnd_armor_var = dnd_armor.choices[0].message["content"]
+    print(dnd_armor_var)
 
-dnd_armor_var = dnd_armor.choices[0].message["content"]
-print(dnd_armor_var)
 
+if  dnd_class_var in ["Fighter", "Barbarian", "Rogue", "Monk", "Paladin", "Ranger"]:
+    dnd_weapon_prompt = f"Using this prompt: '{prompt}' determine a weapon, from Dnd 5e, for this character. The weapon must be less then {gp} gold coins and the output must only be the weapon name, no other words."
+
+    dnd_weapon = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": dnd_weapon_prompt}]
+    )
+
+    if len(dnd_weapon.choices[0].message["content"].split()) > 3:
+        count = 5
+        dnd_weapon_var = dnd_weapon.choices[0].message["content"]
+        while len(dnd_weapon_var.split()) > 3 and count > 0:
+            dnd_weapon_var = dnd_weapon.choices[0].message["content"]
+            count -= 1
+            print(count)
+    else:
+        dnd_weapon_var = dnd_weapon.choices[0].message["content"]
+    print(dnd_weapon_var)
+
+
+if  dnd_class_var in ["Warlock", "Wizard", "Bard", "Cleric", "Artificer", "Sorcerer", "Druid"]:
+    dnd_spell_prompt = f"Using this prompt: '{prompt}' determine the spells a level one {dnd_class_var} would have in Dnd 5e. The output must only be the spell names. No cantrips."
+
+    dnd_spell = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": dnd_spell_prompt}]
+    )
+    
+    dnd_spell_var = dnd_spell.choices[0].message["content"]
+    print(dnd_spell_var)
+   
